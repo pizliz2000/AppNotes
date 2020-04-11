4c9c89a3a62ca7173fe8576d0b02d54de357d85d
Alexandr <betmen2000@inbox.ru>
2020-04-11 20:39:04 +0300

    Initial commit

---
+++ app/src/main/java/com/example/noteskursach/NoteViewHolder.java
@@ -0,0 +1,34 @@
+package com.example.noteskursach;
+
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.cardview.widget.CardView;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class NoteViewHolder extends RecyclerView.ViewHolder {
+
+    View mView;
+    TextView textTitle, textTime;
+    CardView noteCard;
+
+    public NoteViewHolder(@NonNull View itemView) {
+        super(itemView);
+
+        mView = itemView;
+        textTitle = mView.findViewById(R.id.note_title);
+        textTime = mView.findViewById(R.id.note_time);
+        noteCard=mView.findViewById(R.id.note_card);
+    }
+
+    public void setNoteTitle(String title) {
+        textTitle.setText(title);
+    }
+
+    public void setNoteTime(String time) {
+        textTime.setText(time);
+    }
+
+
+}
